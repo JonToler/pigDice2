2400045b0e2207ff295c3287d26312bc4eedd874
Garrett Price and Jon Toler <garrett@>
2016-08-24 15:51:56 -0700

    behavior: adding alert function for winner and for a reset for the game once the winner is chosen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 //bussiness logic
 var pigDice = {
-player1:0,
-player2:0,
-currentPlayer:1,
-turnScore:0,
+  player1: 0,
+  player2: 0,
+  currentPlayer: 1,
+  turnScore: 0,
 };
 
 function rollDice() {
-  var die6 = Math.floor(Math.random()*6) + 1;
+  var die6 = Math.floor(Math.random() * 6) + 1;
   return die6;
 }
 
 function playerTurn() {
   var dieRoll = rollDice();
   if (dieRoll === 1) {
-  pigDice.turnScore = 0;
-  alert("You rolled a Pig! It's the next player's turn!")
-  nextPlayer();
-} else {
-  pigDice.turnScore += dieRoll;
-  if (pigDice.currentPlayer === 1) {
-    if (pigDice.turnScore + pigDice.player1 >= 100) {
-       alert("Player 1 wins!");
-     }
-   } else if (pigDice.turnScore + pigDice.player2 >= 100) {
-      alert("Player 2 wins!");
-   }
- }
-    return dieRoll;
+    pigDice.turnScore = 0;
+    alert("You rolled a Pig! It's the next player's turn!")
+    nextPlayer();
+  } else {
+    pigDice.turnScore += dieRoll;
+    if (pigDice.currentPlayer === 1) {
+      if (pigDice.turnScore + pigDice.player1 >= 100) {
+        alertWinner(1);
+      }
+    } else if (pigDice.turnScore + pigDice.player2 >= 100) {
+      alertWinner(2);
+    }
   }
+  return dieRoll;
+}
 
 function holdButton() {
   var playerUp = pigDice.currentPlayer;
@@ -48,22 +48,34 @@ function nextPlayer() {
     pigDice.currentPlayer = 1;
   }
 }
+
+function resetGame() {
+  pigDice.player1 = 0;
+  pigDice.player2 = 0;
+  pigDice.currentPlayer = 1;
+  pigDice.turnScore = 0;
+}
+
 //UI logic
-$(document).ready(function(){
-$("#roll").click(function(){
+
+function alertWinner(pigFace) {
+  alert("Player" + pigFace + " Wins!");
+  resetGame();
+  $(".gameStatusDisplay").text(0);
+}
+
+$(document).ready(function() {
+$("#roll").click(function() {
   output = playerTurn();
   $("#CurrRollResults").text(output);
   $("#CurrRollScore").text(pigDice.turnScore);
   });
 
-$("#hold").click(function(){
-holdScore = holdButton();
+  $("#hold").click(function() {
+    holdScore = holdButton();
 
-$("#totalPlayer1").text(pigDice.player1);
+    $("#totalPlayer1").text(pigDice.player1);
 
-$("#totalPlayer2").text(pigDice.player2);
-
-
+    $("#totalPlayer2").text(pigDice.player2);
+  });
 });
-});
-//eventPrevent(default);
